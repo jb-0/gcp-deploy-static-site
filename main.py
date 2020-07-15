98f85a5412a2658951c1db6636cc4340361b626b
Jamie Barrett <tbrt92@gmail.com>
2020-07-15 21:51:32 +0100

    Improved structure through functions

--- main.py
+++ main.py
@@ -5,23 +5,32 @@ import git
 import shutil
 import tempfile
 
-storage_client = storage.Client(credentials=credentials, project=project)
-bucket = storage_client.bucket(bucket_name)
 
-temp_dir = tempfile.mkdtemp()
-git.Repo.clone_from('https://github.com/jb-0/site.git', temp_dir, branch='master', depth=1)
+def main():
+    storage_client = storage.Client(credentials=credentials, project=project)
+    bucket = storage_client.bucket(bucket_name)
 
-exclude_subdirs = set(['.git'])
-exclude_files = set(['README.md', 'LICENSE', '.gitignore'])
+    temp_dir = tempfile.mkdtemp()
+    git.Repo.clone_from('https://github.com/jb-0/site.git', temp_dir, branch='master', depth=1)
 
-for path, subdirs, files in os.walk(temp_dir):
-    subdirs[:] = [sd for sd in subdirs if sd not in exclude_subdirs]
-    files[:] = [f for f in files if f not in exclude_files]
-    for name in files:
-        local_path = os.path.join(path, name)
-        blob_path = local_path.replace(temp_dir + '/', '')
+    upload_repo_to_bucket(temp_dir, bucket)
 
-        blob = bucket.blob(blob_path)
-        blob.upload_from_filename(local_path)
+    shutil.rmtree(temp_dir)
 
-shutil.rmtree(temp_dir)
+
+def upload_repo_to_bucket(dir, bucket):
+    exclude_subdirs = set(['.git'])
+    exclude_files = set(['README.md', 'LICENSE', '.gitignore'])
+
+    for path, subdirs, files in os.walk(dir):
+        subdirs[:] = [sd for sd in subdirs if sd not in exclude_subdirs]
+        files[:] = [f for f in files if f not in exclude_files]
+        for name in files:
+            local_path = os.path.join(path, name)
+            blob_path = local_path.replace(dir + '/', '')
+
+            blob = bucket.blob(blob_path)
+            blob.upload_from_filename(local_path)
+
+
+main()
